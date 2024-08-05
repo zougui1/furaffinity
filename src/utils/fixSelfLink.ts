d3f7778a2223399d368dc3c58556a513e248490c
Zougui <zougui.dev@protonmail.com>
2024-08-05 23:12:10 +0200

    normalized URL (removed trailing slash)

--- src/utils/fixSelfLink.ts
+++ src/utils/fixSelfLink.ts
@@ -1,4 +1,8 @@
+import { removeTrailing } from './removeTrailing';
+
 export const fixSelfLink = (selfLink: string) => {
+  selfLink = removeTrailing(selfLink, '/');
+
   if (!selfLink.startsWith('/')) {
     return selfLink;
   }
